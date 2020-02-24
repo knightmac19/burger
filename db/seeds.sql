2c0b0559faffb6578da144cadeb40c592e1d9f95
Patrick Dunn <pmdunn78@gmail.com>
2020-02-23 22:56:03 -0500

    init server.js & .sql files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Rick O'Shea Burger"),
+("Murhpy's Law Burger"),
+("High Steaks Poker Burger"),
+("Supercalifragilisticexpialidoscis Burger"),
+("The 1% Burger"),
+("Moo Burger")
+;
+
+SELECT * FROM burgers;
