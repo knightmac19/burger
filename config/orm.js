54410f22cd013b89b7f53a17364a42b7bb31b6f3
Patrick Dunn <pmdunn78@gmail.com>
2020-02-25 11:41:23 -0500

    wrote orm.js

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+// Import mySQL connection
+var connection = require("../config/connection.js");
+
+// object for all SQL statement functions
+var orm = {
+    // selectAll method
+    selectAll: function(table, cb) {
+        var queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // insertOne method
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = `INSERT INTO ${table} (${cols}) VALUES (${vals});`
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // UPDATE table_reference
+    // SET assignment_list
+    // [WHERE where_condition]
+    // [ORDER BY ...]
+    // [LIMIT row_count]
+
+    // updateOne method
+    updateOne: function(table, cols, condition, cb) {
+        var queryString = `UPDATE ${table} SET ${cols} WHERE ${condition};`
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
